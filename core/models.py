0a1569c00a8038ea44b29ffeec796cf9d50d826d
alirahmanicode <alirahmanicode@gmail.com>
2022-07-04 22:02:14 +0430

    add defualt for review counter

--- core/models.py
+++ core/models.py
@@ -9,7 +9,7 @@ class Vocabulary(models.Model):
     )
     text = models.CharField(max_length=750)
     translation = models.TextField()
-    review_count = models.PositiveIntegerField(blank=True)
+    review_count = models.PositiveIntegerField(blank=True, default=0)
     type = models.CharField(choices=TYEPS_CHOICES, max_length=10)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
