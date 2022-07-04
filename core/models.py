214028051e72177cd9d52cd286efabe64d7e4546
alirahmanicode <alirahmanicode@gmail.com>
2022-07-04 21:49:16 +0430

    create model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Vocabulary(models.Model):
+    TYEPS_CHOICES = (
+        ('word', 'word'),
+        ('phrase', 'phrase')
+    )
+    text = models.CharField(max_length=750)
+    translation = models.TextField()
+    review_count = models.PositiveIntegerField(blank=True)
+    type = models.CharField(choices=TYEPS_CHOICES, max_length=10)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
