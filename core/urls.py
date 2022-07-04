100ccefa956856ec06ece3148e5341bd8bafaf39
alirahmanicode <alirahmanicode@gmail.com>
2022-07-04 22:07:46 +0430

    create add object view

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import Dashboard
+from .views import Dashboard, AddObject
 
 
 app_name = 'core'
 
 urlpatterns = [
-    path('dashboard/', Dashboard.as_view(), name="dashboard")
+    path('dashboard/', Dashboard.as_view(), name="dashboard"),
+    path('add/', AddObject.as_view(), name="add-object")
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from .forms import VocabulayForm
 
-# Create your views here.
+
 class Dashboard(View):
     def get(self, request):
         return render(request, 'core/dashboard.html')
+
+class AddObject(View):
+    def get(self, request, **kwrags):
+        form = VocabulayForm
+        return render(request, 'core/add_object.html', {'form':form})
+
+    def post(self, request, **kwrags):
+        form = VocabulayForm(request.POST)
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.user = request.user
+            obj.save()
+            return redirect(request.META.get('HTTP_REFERER'))
