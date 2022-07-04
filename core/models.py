ec76f48998a3cead620f68de7fe9248b5c0af22d
alirahmanicode <alirahmanicode@gmail.com>
2022-07-04 22:23:31 +0430

    add model manager for vocab model

--- core/models.py
+++ core/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class VocabManager(models.Manager):
+    def get_words(self):
+        return self.get_queryset().filter(type='word')
+
+    def get_phrases(self):
+        return self.get_queryset().filter(type='phrase')
+
 class Vocabulary(models.Model):
     TYEPS_CHOICES = (
         ('word', 'word'),
@@ -14,6 +21,8 @@ class Vocabulary(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    objects = VocabManager()
 
     def __str__(self):
         return self.text[:20]
