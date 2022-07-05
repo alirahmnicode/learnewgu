cc6bd0d5fbd0a5f5d98e169fb36affb8dbdf2ad9
alirahmanicode <alirahmanicode@gmail.com>
2022-07-05 22:45:28 +0430

    create home view

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from .views import Dashboard, AddObject, UpdateVocab
 
 
 app_name = 'core'
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name="index.html"), name="home"),
     path('dashboard/', Dashboard.as_view(), name="dashboard"),
     path('add/', AddObject.as_view(), name="add-object"),
     path('edit/<int:pk>/', UpdateVocab.as_view(), name="update-vocab"),
