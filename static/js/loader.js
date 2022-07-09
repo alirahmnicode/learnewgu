a75b298b9de24437010f8f3e9d31ec356c63eec2
alirahmanicode <alirahmanicode@gmail.com>
2022-07-09 12:00:32 +0430

    compelete loader system

--- static/js/loader.js
+++ static/js/loader.js
@@ -1,29 +1,57 @@
 var myDiv = document.getElementsByClassName('all-article')[0]
 var number = 0
+var host = window.location.host
+
 $(window).scroll(function (event) {
     if ($(window).scrollTop() + $(window).height() >= $(document).height()) {
         number += 15
-        console.log('ali')
+        view_url = `http://${host}/vocab/list/?n=${number}`
         $.ajax({
             type: "GET",
-            url: `http://localhost:8000/vocab/list/?n=${number}`,
+            url: view_url,
             success: function (response) {
                 for (let i = 0; i < response.objects.length; i++) {
-                    var box = `<div>
-                                    <div>${response.objects[i].counter}</div>
-                                    <div>${response.objects[i].text}</div>
-                                    <div>${response.objects[i].translation}</div>
-                                    <div class="view-count">${response.objects[i].review_count}</div>
-                                    <div>${response.objects[i].type}</div>
-                                    <div>${response.objects[i].created}</div>
+                    var box = `<div class="box">
                                     <div>
-                                        <input type="hidden" name="pk" value="${response.objects[i].pk}">
-                                        <button class="viewed">Viewed</button>
+                                        <div class="text mr">
+                                            <div class="mr">${response.objects[i].counter}</div>
+                                            <div class="mr"><span>${response.objects[i].text}</span></div>
+                                            <div class="mr">
+                                                <button class="see">see trns</button>
+                                            </div>
+                                        </div>
+                                        <div class="translation mr">
+                                            <span>${response.objects[i].ttranslationxt}</span>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div class="info">
+                                            <div class="mr">
+                                                <span>type : ${response.objects[i].type}</span>
+                                            </div>
+                                            <div class="mr">
+                                                <span>review count : ${response.objects[i].review_count}</span>
+                                            </div>
+                                        </div>
+                                        <div class="manage">
+                                            <div class="mr">
+                                                <a href="{% url 'core:edit' pk=obj.pk %}">Edit</a>
+                                            </div>
+                                            <div class="mr">
+                                                <a href="{% url 'core:delete' pk=obj.pk %}">Delete</a>
+                                            </div>
+                                            <div class="mr">
+                                                <input type="hidden" name="pk" value="${response.objects[i].pk}">
+                                                <button class="viewed">Viewed</button>
+                                            </div>
+                                        </div>
+                                        <div class="date">
+                                            <span>${response.objects[i].created}</span>
+                                        </div>
                                     </div>
                                 </div>`
                     $('.vocabs').append(box)
                 }
-                console.log(response)
             }
         })
     }
