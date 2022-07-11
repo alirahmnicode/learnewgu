20d46dbe316015f18c2db17ccc3c351715c2e761
alirahmanicode <alirahmanicode@gmail.com>
2022-07-11 09:27:28 +0430

    edit urls

--- core/urls.py
+++ core/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name="index.html"), name="home"),
     path('dashboard/', Dashboard.as_view(), name="dashboard"),
     path('add/', AddObject.as_view(), name="add"),
-    path('edit/<int:pk>/', UpdateVocab.as_view(), name="edit"),
+    path('vocab/<int:pk>/edit/', UpdateVocab.as_view(), name="edit"),
     path('vocab/<int:pk>/delete/', DeleteVocabView.as_view(), name="delete"),
     path('vocab/<int:pk>/detail/', DetailVocabView.as_view(), name="detail"),
     path('vocab/list/', ListVocabView.as_view(), name="list"),
