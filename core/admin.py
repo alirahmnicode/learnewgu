43994442c72bc447a4bf7e5c592466759072503c
alirahmanicode <alirahmanicode@gmail.com>
2022-07-13 08:34:20 +0430

    customize admin

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Vocabulary
 
-admin.site.register(Vocabulary)
+@admin.register(Vocabulary)
+class VocabAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'text', 'type', 'user', 'created', 'review_count')
+    list_filter = ('type', 'review_count',)
+    search_fields = ('user__username', 'text')
