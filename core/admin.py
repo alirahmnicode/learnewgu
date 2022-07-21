87e9c2f2ab4c3046ac5b59ef65c005605a433936
alirahmanicode <alirahmanicode@gmail.com>
2022-07-21 08:45:39 +0430

    add to admin pannel

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Vocabulary
+from .models import Vocabulary, Category
 
 @admin.register(Vocabulary)
 class VocabAdmin(admin.ModelAdmin):
     list_display = ('pk', 'text', 'type', 'user', 'created', 'review_count')
     list_filter = ('type', 'review_count',)
     search_fields = ('user__username', 'text')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user')
