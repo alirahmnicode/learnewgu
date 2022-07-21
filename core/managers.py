2fcb786ed7b845018b74568e186dd7f8fb290c2e
alirahmanicode <alirahmanicode@gmail.com>
2022-07-21 08:41:42 +0430

    create category model and seprate model manager

---
+++ core/managers.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class VocabManager(models.Manager):
+    def get_queryset(self, owner=None):
+        queryset = super().get_queryset().order_by('-created')
+        if owner is not None:
+            return queryset.filter(user=owner)
+        else:
+            return queryset.order_by('-created')
+
+    def all(self, owner=None):
+        return self.get_queryset(owner=owner)
+
+    def get_words(self, owner=None):
+        return self.get_queryset(owner=owner).filter(type='word')
+
+    def get_phrases(self, owner=None):
+        return self.get_queryset(owner=owner).filter(type='phrase')
+
+    def get_recent_obj(self, owner=None):
+        return self.get_queryset(owner=owner)
--- core/models.py
+++ core/models.py
@@ -1,26 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
-
-class VocabManager(models.Manager):
-    def get_queryset(self, owner=None):
-        queryset = super().get_queryset().order_by('-created')
-        if owner is not None:
-            return queryset.filter(user=owner)
-        else:
-            return queryset.order_by('-created')
-
-    def all(self, owner=None):
-        return self.get_queryset(owner=owner)
-
-    def get_words(self, owner=None):
-        return self.get_queryset(owner=owner).filter(type='word')
-
-    def get_phrases(self, owner=None):
-        return self.get_queryset(owner=owner).filter(type='phrase')
-
-    def get_recent_obj(self, owner=None):
-        return self.get_queryset(owner=owner)
+from .managers import VocabManager
 
 
 class Vocabulary(models.Model):
@@ -45,3 +25,13 @@ class Vocabulary(models.Model):
         
     def __str__(self):
         return self.text[:20]
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=300)
+    vocabs = models.ManyToManyField(Vocabulary, related_name='v', verbose_name='Vocabulary')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
