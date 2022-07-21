4f1459de2eb8b70c434d5cf38613b4fab60d8623
alirahmanicode <alirahmanicode@gmail.com>
2022-07-21 08:50:49 +0430

    remove category model

--- core/admin.py
+++ core/admin.py
@@ -1,13 +1,8 @@
 from django.contrib import admin
-from .models import Vocabulary, Category
+from .models import Vocabulary
 
 @admin.register(Vocabulary)
 class VocabAdmin(admin.ModelAdmin):
     list_display = ('pk', 'text', 'type', 'user', 'created', 'review_count')
     list_filter = ('type', 'review_count',)
     search_fields = ('user__username', 'text')
-
-
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'user')
--- core/models.py
+++ core/models.py
@@ -25,13 +25,3 @@ class Vocabulary(models.Model):
         
     def __str__(self):
         return self.text[:20]
-
-
-class Category(models.Model):
-    name = models.CharField(max_length=300)
-    vocabs = models.ManyToManyField(Vocabulary, related_name='v', verbose_name='Vocabulary')
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    created = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.name
