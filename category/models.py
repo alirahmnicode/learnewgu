0e6386f7a1ed9eee110fc24d110aedbe8fea630e
alirahmanicode <alirahmanicode@gmail.com>
2022-07-21 08:54:00 +0430

    create category model and add to admin

--- category/models.py
+++ category/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from core.models import Vocabulary
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=300)
+    vocabs = models.ManyToManyField(Vocabulary, related_name='v', verbose_name='Vocabulary')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
