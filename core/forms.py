7fceb02d37dbb0e816e6ef36817cbba67298463c
alirahmanicode <alirahmanicode@gmail.com>
2022-07-23 10:27:31 +0430

    add attr to text input

--- core/forms.py
+++ core/forms.py
@@ -6,4 +6,7 @@ class VocabulayForm(forms.ModelForm):
     class Meta:
         model = Vocabulary
         fields = ('text', 'translation', 'type')
+        widgets = {
+            'text': forms.TextInput(attrs={'autocomplete':'off'})
+        }
         
