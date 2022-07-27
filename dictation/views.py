fe7004a1199c8d6c915fb261e7135b559a3433bb
alirahmanicode <alirahmanicode@gmail.com>
2022-07-27 16:48:14 +0430

    create filling view

--- dictation/views.py
+++ dictation/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+import random
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404
+from django.views import View
+from core.models import Vocabulary
+from core.ajax import is_ajax
 
-# Create your views here.
+
+class FillingView(View):
+    def get(self, request, **kwargs):
+        words = Vocabulary.objects.filter(type='word')
+        word = random.choice(words)
+        if is_ajax(request):
+            word = word.values('pk', 'text', 'translation',
+                        'type', 'review_count', 'created')
+            return JsonResponse(data=word)
+        else:
+            if kwargs['pk']:
+                word = get_object_or_404(Vocabulary, kwargs['pk'])
+            context = {
+                'word':word
+            }
+            return render(request, 'dictation/fill.html', context)
