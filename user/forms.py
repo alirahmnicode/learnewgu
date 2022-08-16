5e4aeb8f314019ef52faef75b1ce6ecbe4f068d3
alirahmanicode <alirahmanicode@gmail.com>
2022-08-16 13:21:14 +0430

    add attribute to form

--- user/forms.py
+++ user/forms.py
@@ -11,7 +11,7 @@ class UserRegisterForm(forms.Form):
         widget=forms.EmailInput(attrs={"placeholder": "your email"}),
     )
     password = forms.CharField(
-        min_length=6,
+        min_length=4,
         max_length=100,
         widget=forms.PasswordInput(attrs={"placeholder": "your password"}),
     )
@@ -22,10 +22,10 @@ class UserLoginForm(forms.Form):
     username = forms.CharField(
         min_length=3,
         max_length=100,
-        widget=forms.TextInput(attrs={"placeholder": "your username"}),
+        widget=forms.TextInput(attrs={"placeholder": "your username", "value":"admin"}),
     )
     password = forms.CharField(
-        min_length=6,
+        min_length=4,
         max_length=100,
-        widget=forms.PasswordInput(attrs={"placeholder": "your password"}),
+        widget=forms.PasswordInput(attrs={"placeholder": "your password", "value":"admin"}),
     )
