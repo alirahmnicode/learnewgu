20b1edb86fc7c12e94ffcf1c0c1b687b6dadc480
alirahmanicode <alirahmanicode@gmail.com>
2022-12-03 12:01:24 +0330

    delete deep translator

--- translate/views.py
+++ translate/views.py
@@ -1,8 +1,5 @@
 from django.http import JsonResponse
-from django.shortcuts import render
-from .translate import translate_text
 
 
 def translate(request):
-    text = request.GET.get('text')
-    return JsonResponse(translate_text(text, 'fa'), safe=False)
+    pass
